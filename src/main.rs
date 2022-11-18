43e0da1b007bc9ac059ba25606ded1765baa2abb
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-11-18 21:23:13 +0700

    feat: usue enum as platform values

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(version, about)]
+/// Generate a complete and ready-to-use favicons for your websites
 struct Args {
     #[arg(value_name = "source_image", value_hint = clap::ValueHint::DirPath)]
     input: PathBuf,
 
-    #[arg(short = 'p', value_name = "platforms")]
-    platforms: Option<Vec<String>>,
+    #[arg(short = 'p', value_name = "platforms", value_enum)]
+    platforms: Option<Vec<Platform> >,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum Platform {
+    // Favicons that are compatible with almost all web browsers
+    Legacy,
+    // Include favicons features that are supported by major modern browsers
+    Modern,
+    // Enable Android-based favicon support. Includes manifest.
+    Android,
+    // Enable Apple-based device favicon support.
+    Apple,
 }
 
 fn main() {
     let args = Args::parse();
 
     println!("{}", args.input.as_path().to_str().unwrap());
-    println!("{}", args.platforms.unwrap().join(", "))
+
+    let mut platforms = Vec::from([Platform::Legacy, Platform::Modern]);
+
+    if !args.platforms.is_none() {
+        platforms = args.platforms.unwrap();
+    }
+
+    println!("{:?}", platforms[1]);
 }
